2722bbceb0d7b2fb3a46322ec8c1e147678e22a7
Petyaa23 <Petya0023@gmail.com>
2022-09-08 11:42:00 +0400

    Laravel Crop Image Before Upload using Cropper JS task complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,91 +1,77 @@
-// const b64toBlob = (b64Data, contentType='', sliceSize= 512) => {
-//     const byteCharacters = atob(b64Data);
-//     const byteArrays = [];
-//
-//     for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-//         const slice = byteCharacters.slice(offset, offset + sliceSize);
-//
-//         const byteNumbers = new Array(slice.length);
-//         for (let i = 0; i < slice.length; i++) {
-//             byteNumbers[i] = slice.charCodeAt(i);
-//         }
-//
-//         const byteArray = new Uint8Array(byteNumbers);
-//         byteArrays.push(byteArray);
-//     }
-//
-//     const blob = new Blob(byteArrays, {type: contentType});
-//     return blob;
-// }
-//
-// $(document).ready(function(){
-//     $image_crop = $('#image_demo').croppie({
-//         enableExif: true,
-//         viewport: {
-//             width:200,
-//             height:200,
-//             type:"square"
-//         },
-//         boundary:{
-//             width:500,
-//             height:500
-//         }
-//     });
-//
-//     $('#before_crop_image').on('change', function(){
-//         let reader = new FileReader();
-//         reader.onload = function (event) {
-//             $image_crop.croppie('bind', {
-//                 url: event.target.result
-//             }).then(function(){
-//                 console.log('jQuery bind complete');
-//             });
-//         }
-//         reader.readAsDataURL(this.files[0]);
-//
-//         $('#imageModel').modal('show');
-//     });
-//
-//     $('.crop_image').click(function(event){
-//         $image_crop.croppie('result', {
-//             type: 'canvas',
-//             size: 'viewport'
-//
-//         }).then(function(response) {
-//             const formData = new FormData;
-//             formData.append('image', blob);
-//             fetch(response)
-//                 .then(res => res.blob())
-//                 .then(blob => {
-//                     $.ajax({
-//                         url:"/save-crop-image",
-//                         type:'POST',
-//                         data: formData,
-//                         cache : false,
-//                         processData: false,
-//                         contentType: false,
-//                         headers: {
-//                             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//                         },
-//                         success:function(data){
-//                             $('#imageModel').modal('hide');
-//                             window.location.reload();
-//                         }
-//                     })
-//                 })
-//         });
-//     });
-// });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+let $modal = $('#modal');
+let image = document.getElementById('image');
+let cropper;
+
+$("body").on("change", ".image", function(e){
+    let files = e.target.files;
+    let done = function (url) {
+        image.src = url;
+        $modal.modal('show');
+    };
+    let reader;
+    let file;
+    let url;
+
+    if (files && files.length > 0) {
+        file = files[0];
+
+        if (URL) {
+            done(URL.createObjectURL(file));
+        } else if (FileReader) {
+            reader = new FileReader();
+            reader.onload = function (e) {
+                done(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    }
+});
+
+$modal.on('shown.bs.modal', function () {
+    cropper = new Cropper(image, {
+        aspectRatio: 1,
+        viewMode: 3,
+        preview: '.preview'
+    });
+
+}).on('hidden.bs.modal', function () {
+    cropper.destroy();
+    cropper = null;
+});
+
+$("#crop").click(function(){
+    canvas = cropper.getCroppedCanvas({
+        width: 160,
+        height: 160,
+    });
+
+    canvas.toBlob(function(blob) {
+        url = URL.createObjectURL(blob);
+        let reader = new FileReader();
+
+        reader.readAsDataURL(blob);
+        reader.onloadend = function() {
+            let base64data = reader.result;
+
+            const formData = new FormData;
+            formData.append('image', blob);
+
+            $.ajax({
+                url:"/save-crop-image",
+                type:'POST',
+                data: formData,
+                cache : false,
+                processData: false,
+                contentType: false,
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+
+                success:function(data){
+                    $modal.modal('hide');
+                     window.location.reload();
+                }
+            });
+        }
+    });
+})
--- resources/views/image-crop.blade.php
+++ resources/views/image-crop.blade.php
@@ -1,86 +1,9 @@
-{{--<html>--}}
-{{--<head>--}}
+<!DOCTYPE html>
 
-{{--    <title>Cropper</title>--}}
-
-{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
-
-{{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">--}}
-
-{{--    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>--}}
-
-{{--    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>--}}
-
-{{--    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>--}}
-
-{{--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>--}}
-
-{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js"></script>--}}
-
-{{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.css" />--}}
-
-{{--</head>--}}
-{{--<body>--}}
-{{--<div class="container mt-5">--}}
-{{--    <div class="card">--}}
-{{--        <div class="card-header">--}}
-{{--            Crop Image Using jQuery Croppie with Ajax in Laravel--}}
-{{--        </div>--}}
-{{--        <div class="card-body">--}}
-{{--            <input type="file" name="before_crop_image" id="before_crop_image" accept="image/*" />--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
-{{--    @foreach($images as $value)--}}
-{{--        <img alt="" src="{{ $value->title }}">--}}
-{{--    @endforeach--}}
-
-{{--</div>--}}
-
-
-
-{{--<div id="imageModel" class="modal fade bd-example-modal-lg" role="dialog">--}}
-{{--    <div class="modal-dialog modal-lg">--}}
-{{--        <div class="modal-content">--}}
-{{--            <div class="modal-header">--}}
-{{--                <button type="button" class="close" data-dismiss="modal">×</button>--}}
-{{--                <h4 class="modal-title">Crop & Resize Upload Image in PHP with Ajax</h4>--}}
-{{--            </div>--}}
-{{--            <div class="modal-body">--}}
-{{--                <div class="row">--}}
-{{--                    <div class="col-md-8 text-center">--}}
-{{--                        <div id="image_demo" style="width:350px; margin-top:30px"></div>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-md-4" style="padding-top:30px;">--}}
-{{--                        <br />--}}
-{{--                        <br />--}}
-{{--                        <br/>--}}
-{{--                        <button class="btn btn-success crop_image">Save</button>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="modal-footer">--}}
-{{--                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>--}}
-{{--            </div>--}}
-{{--            <div>--}}
-
-
-
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-
-{{--<script src="{{ asset('js/main.js') }}"></script>--}}
-
-{{--</body>--}}
-{{--</html>--}}
-
-    <!DOCTYPE html>
 <html>
 <head>
     <title>Laravel Crop Image Before Upload using Cropper JS</title>
-    <meta name="_token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha256-WqU1JavFxSAMcLP2WIOI+GB2zWmShMI82mTpLDcqFUg=" crossorigin="anonymous"></script>
@@ -108,6 +31,15 @@
     <h1>Laravel Crop Image Before Upload using Cropper JS</h1>
     <input type="file" name="image" class="image">
 </div>
+
+<div style="display: flex">
+
+    @foreach($images as $value)
+        <img alt="" src="{{ $value->title }}" width="100">
+    @endforeach
+
+</div>
+
 
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
@@ -138,95 +70,9 @@
     </div>
 </div>
 
+<script src="{{ asset('js/main.js') }}"></script>
 
-<script>
-
-    let $modal = $('#modal');
-    let image = document.getElementById('image');
-    let cropper;
-
-    $("body").on("change", ".image", function(e){
-        let files = e.target.files;
-        let done = function (url) {
-            image.src = url;
-            $modal.modal('show');
-        };
-        let reader;
-        let file;
-        let url;
-
-        if (files && files.length > 0) {
-            file = files[0];
-
-            if (URL) {
-                done(URL.createObjectURL(file));
-            } else if (FileReader) {
-                reader = new FileReader();
-                reader.onload = function (e) {
-                    done(reader.result);
-                };
-                reader.readAsDataURL(file);
-            }
-        }
-    });
-
-    $modal.on('shown.bs.modal', function () {
-        cropper = new Cropper(image, {
-            aspectRatio: 1,
-            viewMode: 3,
-            preview: '.preview'
-        });
-    }).on('hidden.bs.modal', function () {
-        cropper.destroy();
-        cropper = null;
-    });
-
-    $("#crop").click(function(){
-        canvas = cropper.getCroppedCanvas({
-            width: 160,
-            height: 160,
-        });
-
-        canvas.toBlob(function(blob) {
-
-            url = URL.createObjectURL(blob);
-            let reader = new FileReader();
-
-            reader.readAsDataURL(blob);
-            reader.onloadend = function() {
-                let base64data = reader.result;
-
-                const formData = new FormData;
-                formData.append('image', blob);
-                console.log(formData)
-                $.ajax({
-                    url:"/save-crop-image",
-                    type:'POST',
-                    data: formData,
-                    cache : false,
-                    processData: false,
-                    contentType: false,
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    success:function(data){
-                        $modal.modal('hide');
-                        window.location.reload();
-
-                // $.ajax({
-                //     type: "POST",
-                //     dataType: "json",
-                //     url: "/save-crop-image",
-                //     data: {'_token': $('meta[name="_token"]').attr('content'), 'image': base64data},
-                //     success: function(data){
-                //         $modal.modal('hide');
-                //         alert("success upload image");
-                    }
-                });
-            }
-        });
-    })
-
-</script>
+</body>
+</html>
 
 
